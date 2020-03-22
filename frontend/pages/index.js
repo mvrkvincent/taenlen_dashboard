1d1f84b8c708195b8c9dfcfa4e8cba8dfae93679
Mark Rodriguez <mrkxvncnt@gmail.com>
2020-03-21 22:04:35 -0400

    Splash

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -9,40 +9,17 @@ const Home = () => (
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        This is T&#230;nlen
       </h1>
 
       <p className="description">
-        Get started by editing <code>pages/index.js</code>
+        Coming Spring 2020
       </p>
 
       <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
+        <a href="https://mvrk.dev" className="card">
+          <h3>mvrk.dev &rarr;</h3>
+          <p>Checkout my other work!</p>
         </a>
       </div>
     </main>
@@ -53,7 +30,7 @@ const Home = () => (
         target="_blank"
         rel="noopener noreferrer"
       >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+        Designed & Engineered by Mark Vincent Rodriguez | Brooklyn, NY | Copyright 2020
       </a>
     </footer>
 
@@ -141,8 +118,7 @@ const Home = () => (
         align-items: center;
         justify-content: center;
         flex-wrap: wrap;
-
-        max-width: 800px;
+        width: 50vw;
         margin-top: 3rem;
       }
 
@@ -161,8 +137,7 @@ const Home = () => (
       .card:hover,
       .card:focus,
       .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
+        border-color: #2ED9EB;
       }
 
       .card h3 {
