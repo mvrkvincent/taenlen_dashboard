cf055e3a656e0b881881c59e87e591048fa77124
Mark Rodriguez <mrkxvncnt@gmail.com>
2020-03-22 03:51:45 -0400

    Refactor onClick

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -3,18 +3,22 @@ import React, { useState } from 'react';
 const Nav = () => {
   const [style, setStyle] = useState();
 
+  const handleClick = () => {
+    setStyle({ borderColor: 'red' });
+  };
+
   return (
 
     <nav>
       <div className="section">
         <a href="/" className="logo">&#230;<sup>[1]</sup></a> 
-        <a href="/ledger">Ledger</a>
+        <a href="/ledger">Sheets</a>
         <a href="/blog">Blog</a>
         <a href="/about">About</a>
       </div>
       <div className="section">
-        <a className="auth" onClick={() => setStyle({borderColor: 'red'})} style={style}>Log In</a>
-        <a className="auth" onClick={() => setStyle({ borderColor: 'red' })} style={style}>Sign Up</a>
+        <a className="auth" onClick={() => handleClick()} style={style}>Log In</a>
+        <a className="auth" onClick={() => handleClick()} style={style}>Sign Up</a>
       </div>
     </nav>
 
